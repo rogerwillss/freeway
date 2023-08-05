de9b47fe1fadff043415128741b96516c0c44031
Roger William de Souza Santos <135725677+rogerwillss@users.noreply.github.com>
2023-08-05 02:28:40 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+  
+
+
+function setup(){
+  createCanvas(500, 400);
+    somDaTrilha .loop()
+}
+
+
+function draw(){
+  background(imagemDaEstrada);
+  mostraAtor();
+  movimentaAtor();
+  mostraCarro();
+  movimentaCarro();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluirPontos();
+  marcaPonto();
+ 
+
+  
+  
+}
+
+
+
+
